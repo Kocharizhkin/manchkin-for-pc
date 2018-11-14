a75c3566d7b35fff23474ce09fa0a5a7cdf8a025
Kocharizhkin <egorki_bolshe_net@users.noreply.github.com>
2018-11-14 14:11:58 +0300

    Update Root.py

--- Root.py
+++ Root.py
@@ -1,14 +1,20 @@
 import sys
 import pygame
+from card import Card
 def run_game():
     pygame.init()
     screen=pygame.display.set_mode((900,700))
-    bg_color=(230,230,230)
+    card=Card(screen)
+    bg_color=(120,120,90)
     pygame.display.set_caption("Munchkin")
     while True:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                print(x, y)
         screen.fill(bg_color)
+        card.blitme()
         pygame.display.flip()
 run_game()
